f0a08bc8851baac5abb2be13d43647158e63ff76
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2024-01-19 01:58:27 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
@@ -1,50 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <iomanip>
-#include <numeric>
-
 using namespace std;
 
 int main() {
-    int i, j, C;
-    cin >> C;
-    
-    vector<vector <int>> v_eachLine(C);
-    int eachLine_cnt, score;
-    for(i = 0; i < C; i++)
-    {
-        cin >> eachLine_cnt;
-        for(j = 0; j < eachLine_cnt; j++)
-        {
-            cin >> score;
-            v_eachLine.at(i).push_back(score);
-        }
-    }
+	int c, n, num;
+	int avg;
+	int score[1000] = { 0 };
+	double result;
 
-    vector<double> v_sum;
-    vector<double> v_avg;
-    for(vector<int> a : v_eachLine)
-    {
-        v_sum.push_back(accumulate(a.begin(), a.end(), 0.0));
-        v_avg.push_back(v_sum.back() / a.size());
-    }
+	cin >> c;
+	for (int i = 0; i < c; i++) {
+		avg = 0;
+		num = 0;
+		cin >> n;
 
-    vector<double> results;
-    vector<int> v_over_avg_cnt;
-    int over_avg_cnt = 0;
-    for(i = 0; i < v_eachLine.size(); i++)
-    {
-        for(j = 0; j < v_eachLine[i].size(); j++)
-        {
-            if(v_avg[i] < v_eachLine[i][j])
-                over_avg_cnt++;
-        }
-        v_over_avg_cnt.push_back(over_avg_cnt);
-        over_avg_cnt = 0;
-    }
+		for (int j = 0; j < n; j++) {
+			cin >> score[j];
+			avg = avg + score[j];
+		}
+		avg = avg / n;
+		for (int j = 0; j < n; j++) {
+			if (score[j] > avg)
+				num++;
+		}
+		result = (double)num / n * 100;
 
-    for(int i = 0; i < v_over_avg_cnt.size(); i++)
-        cout << showpoint << setprecision(5) << v_over_avg_cnt[i] / (double)v_eachLine[i].size() * 100 << "%" << endl;
-
-    return 0;
+		cout << fixed;
+		cout.precision(3);
+		cout << result << "%" << endl;
+	}
 }
