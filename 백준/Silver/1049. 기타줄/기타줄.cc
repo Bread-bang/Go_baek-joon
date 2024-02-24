52ba35a99f75a8018e14b0658504642bbafd724d
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2024-02-24 16:17:29 +0900

    [Silver IV] Title: 기타줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.cc
+++ 백준/Silver/1049. 기타줄/기타줄.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,40 +10,21 @@ int main(){
 
     int N; // 끊어진 기타줄의 개수
     int M; // 기타줄 브랜드의 개수
-    vector<pair<int, int> > each_brand_price; // first : package 가격, second : each 가격
+    int package_min = 1001, single_min = 1001;
 
     cin >> N >> M;
-    while(M--)
+
+    for(int i = 0; i < M; i++)
     {
-        int first, second;
-        cin >> first >> second;
-        each_brand_price.emplace_back(make_pair(first, second));
+        int input_package_cost, input_single_min;
+        cin >> input_package_cost >> input_single_min;
+        package_min = min(package_min, input_package_cost);
+        single_min = min(single_min, input_single_min);
     }
-    int total = 0;
-    int min = each_brand_price[0].first;
-    while(N)
-    {
-        if(N >= 6)
-        {
-            for(int i = 0; i < each_brand_price.size(); i++)
-            {
-                min = min > each_brand_price[i].first ? each_brand_price[i].first : min;
-                min = min > (each_brand_price[i].second * 6) ? (each_brand_price[i].second * 6) : min;
-            }
-            total += min;
-            N -= 6;
-        }
-        else
-        {
-            for(int i = 0; i < each_brand_price.size(); i++)
-            {
-                min = min > each_brand_price[i].first ? each_brand_price[i].first : min;
-                min = min > (each_brand_price[i].second * N) ? (each_brand_price[i].second * N) : min;
-            }
-            total += min;
-            N = 0;
-        }
-    }
+
+    int total = (N / 6) * min(package_min, single_min * 6) + min(package_min, single_min * (N % 6));
+    
     cout << total << "\n";
+    
     return 0;
 }
