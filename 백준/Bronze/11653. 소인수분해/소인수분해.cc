10d1ade7851a83a1e132eac14e25ab95f1273fd4
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2024-03-06 15:54:24 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 
 using namespace std;
 
@@ -9,39 +7,19 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    vector<int> prime_candidates;
-
     int N;
     cin >> N;
 
-    int idx = 2;
-    // 에라토스테네스의 체
-    for(int i = 0; i <= N; i++)
-    {
-        prime_candidates.emplace_back(i);
-    }
-    
-    for(int i = idx; i <= N; i++)
-    {
-        if(prime_candidates[i] == 0)
-            continue;
-        
-        for(int j = 2 * i; j <= N; j+=i)
-            prime_candidates[j] = 0;
-    }
-
+    int num = 2;
     while(N != 1)
     {
-        if(N % prime_candidates[idx] == 0)
+        if(N % num == 0)
         {
-            cout << prime_candidates[idx] << "\n";
-            N /= prime_candidates[idx];
+            cout << num << "\n";
+            N /= num;
         }
         else
-        {
-            while(prime_candidates[++idx] == 0)
-                ;
-        }
+            num++;
     }
 
     return 0;
