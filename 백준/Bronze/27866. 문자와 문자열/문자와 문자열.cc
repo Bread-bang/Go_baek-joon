c64b1ec544676a8e0d3b1d538c754b9722e3c334
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2024-08-17 19:42:44 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    int i;
+
+    cin >> word >> i;
+
+    cout << word[i - 1] << "\n";
+
+    return 0;
+}
