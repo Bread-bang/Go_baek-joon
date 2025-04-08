5be3a3688e9f2f3cb9ca4279ba1adfaca9d4d0dd
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2025-04-08 18:07:38 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 220 ms, Memory: 80772 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.go
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.go
@@ -10,18 +10,14 @@ func main() {
 	reader := bufio.NewScanner(os.Stdin)
 	reader.Scan()
 
-	str := []byte(reader.Text())
+	str := reader.Text()
 	length := len(str)
 
 	set := make(map[string]struct{})
 
 	for i := 0; i < length; i++ {
-		firstIdx := 0
 		for j := i; j < length; j++ {
-			partialStr := string(str[firstIdx : j + 1])
-			// fmt.Println("[", firstIdx, j + 1, "]", partialStr)
-			set[partialStr] = struct{}{}
-			firstIdx++
+			set[str[i : j + 1]] = struct{}{}
 		}
 	}
 
