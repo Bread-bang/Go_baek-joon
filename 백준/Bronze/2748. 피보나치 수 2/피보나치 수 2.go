bdf1f95e9d71cfda42645cb0ba5ce7c781235f57
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2025-05-13 16:51:30 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 4 ms, Memory: 864 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.go
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.go
@@ -2,28 +2,17 @@ package main
 
 import "fmt"
 
-var sumArr []int
-
-func fib(n int) int {
-	if n <= 1 {
-		return n 
-	}
-
-	if sumArr[n] != 0 {
-		return sumArr[n]
-	}
-
-	sumArr[n] = fib(n - 1) + fib(n - 2)
-	return sumArr[n]
-}
-
 func main() {
 	n := 0
 	fmt.Scan(&n)
 
-	sumArr = make([]int, n + 1)
+	dp := make([]int, n + 1)
 
-	sumArr[1] = 1
+	dp[1] = 1
 
-	fmt.Println(fib(n))
+	for i := 2; i < n + 1; i++ {
+		dp[i] = dp[i - 1] + dp[i - 2]
+	}
+
+	fmt.Println(dp[n])
 }
