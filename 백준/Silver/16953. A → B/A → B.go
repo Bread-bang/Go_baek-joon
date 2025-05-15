8d6310f6dad58cfc689e2fb9b344819426b0f7f9
Bread-bang <65281350+Bread-bang@users.noreply.github.com>
2025-05-15 23:02:39 +0900

    [Silver II] Title: A → B, Time: 4 ms, Memory: 856 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.go
+++ 백준/Silver/16953. A → B/A → B.go
@@ -11,42 +11,33 @@ import (
 var (
 	r = bufio.NewReader(os.Stdin)
 	w = bufio.NewWriter(os.Stdout)
-	A, B int
 )
-
-func BFS(A int, opCnt int) {
-	queue := [][2]int{}
-
-	queue = append(queue, [2]int{A, opCnt})
-	
-	for len(queue) > 0 {
-		front := queue[0]
-		queue = queue[1:]
-
-		if B == front[0] {
-			fmt.Fprintln(w, front[1])
-			return
-		}
-
-		if front[0] * 2 <= B {
-			queue = append(queue, [2]int{front[0] * 2, front[1] + 1})
-		}
-
-		if front[0] * 10 + 1 <= B {
-			queue = append(queue, [2]int{front[0] * 10 + 1, front[1] + 1})
-		}
-	}
-	
-	fmt.Fprintln(w, -1)
-}
 
 func main() {
 	defer w.Flush()
 
+	var opCnt int
 	line, _ := r.ReadString('\n')
 	each := strings.Fields(line)
-	A, _ = strconv.Atoi(each[0])
-	B, _ = strconv.Atoi(each[1])
+	A, _ := strconv.Atoi(each[0])
+	B, _ := strconv.Atoi(each[1])
 
-	BFS(A, 1)
+	for B >= A {
+		if B == A {
+			fmt.Fprintln(w, opCnt + 1)
+			return
+		}
+
+		if B % 2 == 0 {
+			B /= 2
+			opCnt++
+		} else if B % 10 == 1 {
+			B /= 10
+			opCnt++
+		} else {
+			break
+		}
+	}
+
+	fmt.Fprintln(w, -1)
 }
